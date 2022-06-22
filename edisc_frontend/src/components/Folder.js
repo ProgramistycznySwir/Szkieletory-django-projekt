ceaae5b72caaca385d5858d63680756e91b878ee
Programistyczny Świr <programistycznyswir@gmail.com>
2022-06-22 02:18:15 +0200

    AAAAAAAAAAAAAAAAAAA
    Co-authored-by: Michał Kuczyński <Kucznsky@users.noreply.github.com>

---
+++ edisc_frontend/src/components/Folder.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Directory = () => {
+  return (
+    <div>Foldery i pliki</div>
+  )
+}
+
+export default Directory
---
+++ edisc_frontend/src/components/User.js
@@ -0,0 +1,19 @@
+import { useParams } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
+
+const User = () => {
+    const { id } = useParams();
+    const [user, setUser] = useState([]); 
+
+    useEffect(() => {
+        Axios.get(`http://127.0.0.1:8000/polls/api/profile/${id}`)
+            .then(res => setUsername(res.data));
+    }, []);
+    
+    return (
+        <div>User</div>
+    )
+}
+
+export default User
